8bcbe29f08f6acf4b42a3733fa88e22b175ff60f
muhanpixie <22084931+muhanpixie@users.noreply.github.com>
2021-01-13 02:04:00 -0800

    init?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import * as PIXI from "pixi.js-legacy";
+import icon from "./media/icon.png";
+
 const app = new PIXI.Application({
-  backgroundColor: 0xff0000
+  backgroundColor: 0x1f3f1f
 });
 document.body.appendChild(app.view);
+
+const sp = new PIXI.Sprite.from(icon);
+
+sp.anchor.set(0.5);
+sp.x = app.screen.width/2;
+sp.y = app.screen.height/2;
+
+app.stage.addChild(sp);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 const path = require('path');
+function re_(arg){
+  try{
+    return require(arg);
+  }catch(e){
+    return {};
+  }
+}
+const cfg = re_('./settings.js');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -6,9 +15,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
   devServer: {
-    port: 8052,
-    host: "192.168.0.6",
-    contentBase: path.join(__dirname, 'dist')
+    port: cfg.port || 8052,
+    host: cfg.host || "192.168.0.6",
+    // contentBase: path.join(__dirname, 'dist')
+    contentBase: path.join(__dirname)
   }
 }
